7b7fb8093538a55123b53bf9a76dffbe6d9bf1d5
naresh <nareshsiricilla@gmail.com>
2025-09-06 08:39:11 +0530

    emmeregency added changes and fixed into app.js file

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = required('expression');
+
+function greet() {
+    console.log("Hello app.js");
+}
+
+function multiply(a,b){
+    return a*b;
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+Team Collaboration – Missing Changes
